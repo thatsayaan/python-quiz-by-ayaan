4c12f6a096f0abc33d367444b31c1e9ac226d5c6
Ayaan Masood <145443724+thatsayaan@users.noreply.github.com>
2024-02-16 22:32:21 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,92 +1,120 @@
-#main.py
-questions= ("Q1. How do you insert COMMENTS in Python code?: ",
-            "Q2. How do you create a list in Python?: ",
-            "Q3. Who developed Python Programming Language?: ",
-            "Q4. Which type of Programming does Python support?: ",
-            "Q5. Which of the following is the correct extension of the Python file?: ",
-            "Q6. Which keyword is used for function in Python language?:  ",
-            "Q7. Which of the following is the truncation division operator in Python?: ",
-            "Q8. What will be the output of the following Python code? x='abcd'"
-                 "for i in x:"
-                 "print(i.upper()) : ",
-            "Q9. Which one of the following is not a keyword in Python language?: ",
-            "Q10. Which of the following is a Python tuple?: ",
-            "Q11. Which of these in not a core data type?: ",
-            "Q12. Select all options that print----  hello-how-are-you . ",
-            "Q13. What will be the output of the following Python code?----print (''abc DEF''.capatalize()) : ",
-            "Q14. What will be the output of the following Python expression?    round(4.576): ",
-            "Q15. A while loop in Python is used for what type of iteration?: ",
-            "Q16. Which one is NOT a legal variable name?: ",
-            "Q17. Which method can be used to return a string in upper case letters?: ",
-            "Q18. Which method can be used to replace parts of a string?: ",
-            "Q19. Which collection does not allow duplicate members?: ",
-            "Q20. How do you start writing an if statement in Python?: ")
+#DataFlair-  Import library
+import tkinter as tk
+import random
+from functools import partial
 
-options =(  ("A. $ ","B. comments","C. # ","D. * "),
-            ("A. By enclosing items in square brackets [] ","B. By using parentheses () ","C. By using curly braces {}","D. By using angle brackets <> "),
-            ("A. Wick van Rossum ","B. Rasmus Lerdorf ","C. Ayaan Masood","D. Guido van Rossum"),
-            ("A. structured programming ","B. object-oriented programming ","C. functional programming ","D. all of the mentioned "),
-            ("A. (.python) ","B. (.py) ","C. (.p) ","D. (ide) "),
-            ("A. Function ","B. int ","C. main ","D. def "),
-            ("A. - ","B. % ","C. // ","D. / "),
-            ("A. abcd ","B. ABcD ","C. ABCD ","D. all of the above "),
-            ("A. pass","B. eval ","C. assert ","D. nonlocal "),
-            ("A. {11,32,3} ","B. [1,2,3] ","C. (1,5,7) ","D. {} "),
-            ("A. Lists ","B. Tuples ","C. Dictionary ","D. Class "),
-            ("A. print(‘hello’, ‘how’, ‘are’, ‘you’) ","B. print(‘hello-‘ + ‘how-are-you’) ","C. print(‘hello’ + ‘-‘ + ‘how’ + ‘-‘ + ‘are’ + ‘you’) ","D. print(‘hello’, ‘how’, ‘are’, ‘you’ + ‘-‘ * 4)"),
-            ("A. abc def ","B. ABC DEF ","C. Abc Def","D. Abc def "),
-            ("A. 5 ","B. 4.6 ","C. 4 ","D. 4.5 "),
-            ("A. discriminant ","B. indefinite ","C. definite","D. indeterminate"),
-            ("A. _myvar ","B. my-var ","C. Myvar ","D. my_var"),
-            ("A. upperCase() ","B. toUpperCase() ","C. uppercase() ","D. upper() "),
-            ("A. switch() ","B. repl() ","C. replaceString() ","D. replace() "),
-            ("A. SET ","B. List ","C. Tuple ","D. String "),
-            ("A. if(x>y) ","B. if x>y: ","C. if x>y then ","D. (x>y)if "))
+# DataFlair -  Initializing the window
+window = tk.Tk()
+window.configure(bg="beige")
+window.geometry("800x600")
+window.title('Ayaan -Block game')
 
-answers = ("C","A","D","B","B","D","C","A","B","C","D","B","D","A","B","B","D","D","A","B")
-guesses=[]
-score = 0
-question_num = 0
+Nav = tk.Frame(master=window, pady=5, padx=16)
+Main = tk.Frame(master=window, pady=16, padx=16)
+Footer = tk.Frame(master=window, pady=16, padx=16)
+Nav.pack(expand=True)
+Main.pack(expand=True)
+Footer.pack(expand=True)
 
-for question in questions:
-         print("----------------------------------------------------------------")
-         print("----------------------------------------------------------------")
-         print(question)
+# button creation
+Buttons = []
+Score = 25
+Match_btn = []
+Guesses = 0
 
-         for option in options[question_num]:
-
-             print(option)
-         print("----------------------------------------------------------------")
-         print("----------------------------------------------------------------")
-         guess = input("                   YOUR ANSWER : ").upper()
-         guesses.append(guess)
-         if guess == answers[question_num]:
-             score+=1
-             print("CORRECT")
-         else:
-             print("INCORRECT")
-             print(f"{answers[question_num]}  is the correct answer")
-         question_num += 1
+Label_Score = tk.Label(master=Nav, text=f"Score: {Score}", font=("Times new roman", 30))
+Label_Score.pack()
+Reset_btn = tk.Button(master=Footer, text="Reset", font=("Times new roman", 30), padx=10, pady=3)
+Reset_btn.pack()
 
 
-print("-------------------------------------")
-print("             RESULT                  ")
-print("-------------------------------------")
+def Activate(btn: object, color: str):
+    global Match_btn
+    global Score
+    global Match_btn
+    global Label_Score
+    global Label_Lost
+    global Guesses
 
-print(" correct answers: ",end="")
-for answer in answers:
-    print(answer,end=" ")
-print()
+    if btn['background'] == color:
+        btn['background'] = 'black'
+        Match_btn.pop()
 
-print(" your guesses: ",end="")
-for guess in guesses:
-    print(guess,end=" ,")
-print()
+    else:
+        btn['background'] = color
+        Match_btn.append(btn)
 
-score=score/len(questions)*100
-print(f"your score is:{score}%")
-print("-----------------------------------------------------------------------------------------------------------------------------------------------")
+    if len(Match_btn) == 2:
+        if Match_btn[0]['background'] == Match_btn[1]['background']:
+            Match_btn[0].config(command='')
+            Match_btn[1].config(command='')
+            Match_btn[0]['text'] = color
+            Match_btn[1]['text'] = color
+            Guesses += 1
+        else:
+            Match_btn[0]['background'] = 'black'
+            Match_btn[1]['background'] = 'black'
+            Score -= 1
+            Score_Update(Score)
+
+            if Score == 0:
+                for btn in Buttons:
+                    btn.destroy()
+
+                Label_Lost.pack(side=tk.TOP, expand=True, fill=tk.BOTH)
+
+        if Guesses == 8:
+            for btn in Buttons:
+                btn.destroy()
+
+            Label_Won.pack(side=tk.TOP, expand=True, fill=tk.BOTH)
+
+        Match_btn.clear()
+
+def Game():
+    global Score
+    global Guesses
+    global Label_Score
+    global Buttons
+    global Match_btn
+    global Label_Lost
+    global Label_Won
+    global Nav
+
+    Match_btn = []
+    Guesses = 0
+    Score = 25
+    Colors = ['white', 'orange', 'red', 'green', 'blue', 'cyan', 'yellow', 'purple'] * 2
+
+    Score_Update(Score)
+
+    if len(Buttons):
+        Label_Won.destroy()
+        Label_Lost.destroy()
+        for btn in Buttons:
+            btn.destroy()
+
+    Label_Won = tk.Label(master=Main, text="You WON!", font=("Times new roman", 35))
+    Label_Lost = tk.Label(master=Main, text="You Lost, try again!", font=("Times new roman", 35), )
+
+    for i in range(4):
+        for j in range(4):
+            Random_Color = random.randint(0, len(Colors) - 1)
+            color = Colors[Random_Color]
+
+            Square_btn = tk.Button(master=Main,text=f'?????????', width=10, height=3, background='black',
+                                   activebackground=color)
+
+            Buttons.append(Square_btn)
+            Square_btn.grid(row=i, column=j, padx=10, pady=10)
+            Buttons[-1]['command'] = partial(Activate, Square_btn, color)
+            Colors.pop(Random_Color)
 
 
+def Score_Update(Score: int):
+    Label_Score['text'] = f"Score: {Score}"
 
 
+Reset_btn['command'] = Game
+Game()
+window.mainloop()
